111bdc1899365b21b56bbc881a6cc9a247ff750a
Victor Williams <victor.williams1030@gmail.com>
2020-05-06 14:32:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,7 @@
-phrase = "I LOVE YOU GRANDMA!"
-
-def speak_to_grandma(phrase)
-if phrase = "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
+  puts phrase
+if phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-elsif phrase.downcase
-  puts "HUH?! SPEAK UP, SONNY!"
 else 
   puts "NO, NOT SINCE 1938!"
 end
