98cda4a03341291e59b1b10e945530fb2b1cdc63
Victor Williams <victor.williams1030@gmail.com>
2020-05-06 14:35:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
   puts phrase
 if phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-else 
+elsif phrase == "#{phrase.upcase}"
   puts "NO, NOT SINCE 1938!"
 end
 end
